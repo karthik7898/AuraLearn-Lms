c8a58e72241aead81f08c9ab31786329cbc6abf6
karthik7898 <chukka.karthik2805@gmail.com>
2026-09-21 11:00:14 +0530

    Intial

---
+++ Server/app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/welcome",(req,res)=>{
+    res.send("Welcome back")
+})
+
+
+app.listen(3000, ()=>{
+    console.log("Listening to the PORT");
+    
+})
